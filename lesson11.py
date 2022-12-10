968db6532b27b29cabd1bd68842eec5c391a8816
ASergeyShabe <workshtes@gmail.com>
2022-12-10 17:50:49 +0200

    Готов

--- lesson11.py
+++ lesson11.py
@@ -5,16 +5,18 @@ l = {'Иванов': ['Иван', '+380992364622', 'ivan@gmail.com'],
      'Седов': ['Роман', '+380991279629', 'sedov@ukr.net']}
 l1 = dict(sorted(l.items(), key=lambda i: i[1]))
 print(l1)
-fio = list(input('Введите Фамилию по которым хотите найти данные'))
+fio = input('Введите Фамилию по которым хотите найти данные')
 def BinarySearch(li, key):
     leftInd = 0
     rightInd = len(li) - 1
+    l1 = list(li.keys())
     while leftInd <= rightInd:
         i = leftInd + (rightInd - leftInd) // 2
-        if li[i] == key:
-            print('Найдено по Фамилии ', key, 'следующие ', l1[i])
+        if l1[i] == key:
+            j = l1[i]
+            print('Найдено по Фамилии ', key, 'следующие ', li[j])
             return i
-        elif li[i] < key:
+        elif l1[i] < key:
             leftInd = i + 1
         else:
             rightInd = i - 1
@@ -23,10 +25,10 @@ def BinarySearch(li, key):
 BinarySearch(l1, fio)
 
 
-for j in l1:
-    if j == fio:
-        print('Найдено по Фамилии ', fio, 'следующие ', l1[j])
-        break
-    else:
-        print('Введенной Фамилии нет', fio)
+#for j in l1:
+ #   if j == fio:
+ #       print('Найдено по Фамилии ', fio, 'следующие ', l1[j])
+#        break
+#    else:
+#        print('Введенной Фамилии нет', fio)
 
