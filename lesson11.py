0c6e61242247ce1e1a7f8de28642ec973c99cef5
ASergeyShabe <workshtes@gmail.com>
2022-12-10 13:21:41 +0200

    словарь + список + сортировка по возрастанию получилось

--- lesson11.py
+++ lesson11.py
@@ -1,17 +1,10 @@
-liist = [['Иванов', 'Иван', '+380992364622', 'ivan@gmail.com'],
-      ['Сидоров', 'Сергей', '+380931545712', 'sidov@ukr.net'],
-      ['Петров', 'Артем', '+380501392227', 'petrov@gmail.com'],
-      ['Васичкин', 'Александр', '+3806603762820', 'vasichkin@gmail.com'],
-      ['Седов', 'Роман', '+380991279629', 'sedov@ukr.net']]
-def SortInsert(sorting_list, start = 1):
-    for i in range(start, len(sorting_list)):
-        for j in range(i, 0, -1):
-            if sorting_list[j] < sorting_list[j-1]:
-                sorting_list[j], sorting_list[j-1] = sorting_list[j-1], sorting_list[j]
-            else:
-                break
-SortInsert(liist)
-print(liist)
+l = {'Иванов': ['Иван', '+380992364622', 'ivan@gmail.com'],
+     'Сидоров': ['Сергей', '+380931545712', 'sidov@ukr.net'],
+     'Петров': ['Артем', '+380501392227', 'petrov@gmail.com'],
+     'Васичкин': ['Александр', '+3806603762820', 'vasichkin@gmail.com'],
+     'Седов': ['Роман', '+380991279629', 'sedov@ukr.net']}
+l1 = dict(sorted(l.items(), key=lambda x: x[1]))
+print(l1)
 fio = input('Введите Фамилию по которым хотите найти данные')
 def BinarySearch(li, key):
       leftInd = 0
@@ -23,10 +16,11 @@ def BinarySearch(li, key):
             if li[i] == key:
                   print('k:', k)
                   return i
-            elif li[i] < key:
-                  leftInd = i + 1
             else:
+                if key < li[i]:
+                  leftInd = i + 1
+                else:
                   rightInd = i - 1
       print('k:', k)
       return None
-print('по Фамилии', fio, 'следующие данные', BinarySearch(liist, fio))
+print('по Фамилии', fio, 'следующие данные', BinarySearch(l, fio))
