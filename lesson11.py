04f5d589bfeb788adc025e7fdbcd0f33cc8fd784
ASergeyShabe <workshtes@gmail.com>
2022-12-10 16:53:54 +0200

    словарь + список + сортировка по возрастанию получилось + линейный поиск

--- lesson11.py
+++ lesson11.py
@@ -3,24 +3,30 @@ l = {'Иванов': ['Иван', '+380992364622', 'ivan@gmail.com'],
      'Петров': ['Артем', '+380501392227', 'petrov@gmail.com'],
      'Васичкин': ['Александр', '+3806603762820', 'vasichkin@gmail.com'],
      'Седов': ['Роман', '+380991279629', 'sedov@ukr.net']}
-l1 = dict(sorted(l.items(), key=lambda x: x[1]))
+l1 = dict(sorted(l.items(), key=lambda i: i[1]))
 print(l1)
-fio = input('Введите Фамилию по которым хотите найти данные')
+fio = list(input('Введите Фамилию по которым хотите найти данные'))
 def BinarySearch(li, key):
-      leftInd = 0
-      rightInd = len(li) - 1
-      k = 0
-      while leftInd <= rightInd:
-            k += 1
-            i = leftInd + (rightInd - leftInd) // 2
-            if li[i] == key:
-                  print('k:', k)
-                  return i
-            else:
-                if key < li[i]:
-                  leftInd = i + 1
-                else:
-                  rightInd = i - 1
-      print('k:', k)
-      return None
-print('по Фамилии', fio, 'следующие данные', BinarySearch(l, fio))
+    leftInd = 0
+    rightInd = len(li) - 1
+    while leftInd <= rightInd:
+        i = leftInd + (rightInd - leftInd) // 2
+        if li[i] == key:
+            print('Найдено по Фамилии ', key, 'следующие ', l1[i])
+            return i
+        elif li[i] < key:
+            leftInd = i + 1
+        else:
+            rightInd = i - 1
+
+    return print('Введенной Фамилии нет', fio)
+BinarySearch(l1, fio)
+
+
+for j in l1:
+    if j == fio:
+        print('Найдено по Фамилии ', fio, 'следующие ', l1[j])
+        break
+    else:
+        print('Введенной Фамилии нет', fio)
+
